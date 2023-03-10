dd96c44941a0e47900e08a23d561d1e0cf6471be
Saba Madadi <sabamadadi9@gmail.com>
2023-03-10 11:49:39 +0330

    Update User.java

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,38 +1,12 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class User {
-    static String name;
-    static ArrayList<String> books = new ArrayList<String>();
-    static Library library;
-
-    public User(String username, Library input_library) {
-        name = username;
-        library = input_library;
-    }
+    //User should have a list of books
+    //User should have a username and a password
 
     public void rentBook(){
-        Scanner cs = new Scanner(System.in);
-        System.out.println("Enter name of the book:");
-        String name = cs.nextLine();
-        if(library.doesBookExist(name)) {
-            library.decreaseBook(name);
-            books.add(name);
-        }
-        return;
+        //TODO
     }
 
     public void returnBook(){
-        Scanner cs = new Scanner(System.in);
-        System.out.println("Enter name of the book:");
-        String name = cs.nextLine();
-        for(int i = 0; i < books.size(); i++) {
-            if(books.get(i).equals(name)) {
-                books.remove(i);
-                library.increaseBook(name);
-                return;
-            }
-        }
-        return;
+        //TODO
     }
 }
