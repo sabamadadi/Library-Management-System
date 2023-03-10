0f8bacda560d0345300117a01b8e52c0ac882c4c
Saba Madadi <sabamadadi9@gmail.com>
2023-03-10 12:11:12 +0330

    Update Book.java

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,14 +1,10 @@
-public class Book {
-    //Book should contain name,author,year of publish and ISBN
-    static String name;
-    static String author;
-    static int yop;
-    static int cnt;
+public class Librarian {
+    /*
+    * The librarian should have a username and a password
+    * The librarian should be able to search users, librarians and books
+    * The librarian should be able to add\remove\update user add\remove\update_
+    _ librarian and add\remove\update book
+     */
 
-    public Book(String input_name, String input_author, int input_yop, int input_cnt) {
-        name = input_name;
-        author = input_author;
-        yop = input_yop;
-        cnt = input_cnt;
-    }
+
 }
